e2ce88a249e5aba11ae2dca5bc5a54eed33a968b
VandanaJR <78820142+VandanaJR@users.noreply.github.com>
2021-05-22 13:20:42 +0530

    minor changes

--- src/component/sidenav/sidenav.jsx
+++ src/component/sidenav/sidenav.jsx
@@ -15,9 +15,9 @@ import Data from '../fetchData'
 import request from '../fetchRequestType'
 
 //Imports for Redux
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {setPopularMovies, setPopularDramas} from '../../state/shows-data'
-import {setPage} from '../../state/homepage'
+
 
 const SideNav = ()=>{
 
@@ -31,39 +31,19 @@ const SideNav = ()=>{
     //const statePopularDramas = useSelector(state => state.rootReducer.showsData.popularDramas)
     //console.log(statePopularDramas)
 
-    const home = useSelector(state => state.rootReducer.homePage.home)
-    const movies = useSelector(state => state.rootReducer.homePage.movies)
-    const tv = useSelector(state => state.rootReducer.homePage.tv)
-    const search = useSelector(state => state.rootReducer.homePage.search)
+   
     const location = useLocation().pathname
-    //console.log(tv)
-    const handleClick= (e)=>{
-        let target= e.currentTarget.attributes['name'].value
-        //console.log(e.target)
-        
-        if(target === "home" && location ==='/' ){
-            dispatch(setPage(target))
-        }
-        else if(target==="movies" && location ==='/movies' ){
-            dispatch(setPage(target))
-        }
-        else if(target==="tv" && location ==='/series' ){
-            dispatch(setPage(target))
-        }
-        else if(target==="search" && location ==='/search' ){
-            dispatch(setPage(target))
-        }
-        
-    }
+
+   
     return(
         <div className="sidenav-container">
             <Logo  style={{width:"40px",height:"40px"}}></Logo>  
 
             <Link to="/">
                 <Tooltip  title="Home">
-                    <IconButton name="home" onClick={(e)=>handleClick(e)}>
+                    <IconButton name="home">
                     { 
-                    home && location ==='/'?
+                    location ==='/'?
                     <HomeIcon style={{color:"#e75a61"}} ></HomeIcon>
                     :
                     <HomeIcon className="nav-icon" ></HomeIcon>
@@ -75,9 +55,9 @@ const SideNav = ()=>{
 
             <Link to="/movies">
                 <Tooltip  title="Movies">
-                    <IconButton name="movies" onClick={(e)=>handleClick(e)}>
+                    <IconButton name="movies">
                     { 
-                    movies || location ==='/movies'?
+                   location ==='/movies'?
                     <MovieIcon style={{color:"#e75a61"}} ></MovieIcon>
                     :
                     <MovieIcon className="nav-icon" ></MovieIcon>
@@ -90,9 +70,9 @@ const SideNav = ()=>{
 
             <Link to="/series">
                 <Tooltip  title="TV">
-                    <IconButton name="tv" onClick={(e)=>handleClick(e)}>
+                    <IconButton name="tv" >
                     { 
-                    tv || location ==='/series'?
+                    location ==='/series'?
                     <TvIcon style={{color:"#e75a61"}} ></TvIcon>
                     :
                     <TvIcon className="nav-icon" ></TvIcon>
@@ -105,9 +85,9 @@ const SideNav = ()=>{
 
             <Link to="/search">
                 <Tooltip  title="Search">
-                    <IconButton name="search" onClick={(e)=>handleClick(e)}>
+                    <IconButton name="search" >
                     { 
-                    search || location ==='/search'?
+                    location ==='/search'?
                     <SearchIcon style={{color:"#e75a61"}} ></SearchIcon>
                     :
                     <SearchIcon className="nav-icon" ></SearchIcon>
--- src/state/root-reducer.js
+++ src/state/root-reducer.js
@@ -1,10 +1,8 @@
 import {combineReducers} from 'redux'
 import showDataReducer from './shows-data'
-import HomePageReducer from './homepage'
 
  export default combineReducers({
    showsData :showDataReducer, 
-   homePage: HomePageReducer
 })
 
 
